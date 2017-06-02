4f7f10a8b9118dd5fcab864fb45ff479cb7fc2e6
David Miotti <david.miotti@wopata.com>
2017-06-02 17:41:34 +0200

    Can add a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,11 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private handleError(error: any): Promise<any> {
+    console.error('An error has occured', error);
+    return Promise.reject(error.message || error);
+  }
   constructor(private http: Http) { }
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl).toPromise()
@@ -20,8 +25,17 @@ export class HeroService {
       .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
-  private handleError(error: any): Promise<any> {
-    console.error('An error has occured', error);
-    return Promise.reject(error.message || error);
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}}`;
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+  create(name: string): Promise<Hero> {
+    return this.http.post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
 }
